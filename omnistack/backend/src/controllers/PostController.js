41e630542aeab6d2996151b2aea8aaa2fb08a84e
Carlos Bridi <carlosbridi@gmail.com>
2019-12-17 17:12:39 -0300

    Create struct for new post and like of post, new dependencies and controllers

--- omnistack/backend/src/controllers/PostController.js
+++ omnistack/backend/src/controllers/PostController.js
@@ -1,12 +1,38 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require ('fs');
 
 module.exports = {
     async index(req, res) {
-
+        const posts = await Post.find().sort('-createAt');
+        return res.json(posts);
     },
 
     async store(req, res) {
-        console.log(req.file);
-        return res.json({ok: true});
+        
+        const {author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
+
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({quality: 70})
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            )
+
+        const post = await Post.create({
+            author,
+            description,
+            place,
+            hashtags,
+            image: fileName,
+        });
+
+        req.io.emit('post', post);
+
+        return res.json(post);
     }
 };
--- omnistack/backend/src/index.js
+++ omnistack/backend/src/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const moongose = require('mongoose');
-
+const cors = require('cors');
+const path = require('path');
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 moongose.connect('mongodb+srv://omnistack:omnistack@cluster0-8lr40.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
